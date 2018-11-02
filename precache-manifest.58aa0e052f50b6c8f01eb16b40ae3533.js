72d0d1b3602051b33ea826ce532c16bda179219a
deepskyper <appl.ms.bing@gmail.com>
2018-11-02 13:29:05 -0500

    deploy

---
+++ precache-manifest.58aa0e052f50b6c8f01eb16b40ae3533.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7edb812a958def0057cf2f62c0ee6cf1",
+    "url": "/static/media/welcome_card.7edb812a.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "474ff45f564a4d7682af",
+    "url": "/static/js/main.474ff45f.chunk.js"
+  },
+  {
+    "revision": "cc886c573702ab29adde",
+    "url": "/static/js/1.cc886c57.chunk.js"
+  },
+  {
+    "revision": "474ff45f564a4d7682af",
+    "url": "/static/css/main.3acad12b.chunk.css"
+  },
+  {
+    "revision": "cc886c573702ab29adde",
+    "url": "/static/css/1.4ce9d51b.chunk.css"
+  },
+  {
+    "revision": "fd536e9887f4a30de9e67968ad11d11d",
+    "url": "/index.html"
+  }
+];
