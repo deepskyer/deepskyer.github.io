b543c13d47f1176483e7ea8cc3f4bb438d919698
deepskyer <appl.ms.bing@gmail.com>
2018-11-16 20:37:27 -0600

    update card style

---
+++ precache-manifest.d26bdda7d05f5d993a33881ddc066c80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7edb812a958def0057cf2f62c0ee6cf1",
+    "url": "/static/media/welcome_card.7edb812a.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "673be245d87d24fbf2e4",
+    "url": "/static/js/main.673be245.chunk.js"
+  },
+  {
+    "revision": "2ca920b8550a6a239282",
+    "url": "/static/js/1.2ca920b8.chunk.js"
+  },
+  {
+    "revision": "673be245d87d24fbf2e4",
+    "url": "/static/css/main.7075ad7d.chunk.css"
+  },
+  {
+    "revision": "2ca920b8550a6a239282",
+    "url": "/static/css/1.7f9d7091.chunk.css"
+  },
+  {
+    "revision": "b74c99a86ef4395778cb23a4f37f63a1",
+    "url": "/index.html"
+  }
+];
