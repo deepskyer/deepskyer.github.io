f7485de8c07d10d105b0f0c809815d8b5adf9aed
deepskyer <44412623+deepskyer@users.noreply.github.com>
2020-09-26 15:58:57 -0500

    Add files via upload

--- assets/styles.js
+++ assets/styles.js
@@ -1,16 +1,4 @@
 window.addEventListener("load", function (event) {
-  var searchs = document.getElementsByClassName("search");
-  for (let index = 0; index < searchs.length; index++) {
-    const element = searchs[index];
-    element.classList.remove("float-right");
-    element.classList.remove("btn-group");
-    alert("remove float right");
-  }
-
-  //   var control = document.getElementsByClassName("form-control");
-  //   var control = control[0];
-  //   control.classList.add("d-search");
-
   var selects = document.getElementsByClassName("custom-select");
   for (let index = 0; index < selects.length; index++) {
     const element = selects[index];
