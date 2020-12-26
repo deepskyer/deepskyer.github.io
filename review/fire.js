f74c20d4726a3108f071dc34daab9749c472f0a0
deepskyer <44412623+deepskyer@users.noreply.github.com>
2020-12-25 22:45:02 -0600

    Add files via upload

---
+++ review/fire.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: "AIzaSyAUk_8pzDM9VOqB73TJiJrNZDXuQPRvVX4",
+  authDomain: "codetime-8fc2d.firebaseapp.com",
+  databaseURL: "https://codetime-8fc2d-default-rtdb.firebaseio.com",
+  projectId: "codetime-8fc2d",
+  storageBucket: "codetime-8fc2d.appspot.com",
+  messagingSenderId: "148366966164",
+  appId: "1:148366966164:web:f739792272d82b34b96381",
+  measurementId: "G-3KGSBG4V36",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
